4e468662ce3588c0ca7e0b4bd16b4b18da777ca4
Wickie Lee <wickie.leejr@gmail.com>
2020-05-27 20:32:36 -0400

    regen handle.go agaaaain

--- regen/op/handle.go
+++ regen/op/handle.go
@@ -11,559 +11,559 @@ func HandleOp(buf []byte) int {
 	case 0x00:
 		fmt.Printf("NOP\n")
 	case 0x01:
-		fmt.Printf("LXI\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("LXI     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x02:
-		fmt.Printf("STAX\tB\n")
+		fmt.Printf("STAX    B\n")
 	case 0x03:
-		fmt.Printf("INX\t\tB\n")
+		fmt.Printf("INX     B\n")
 	case 0x04:
-		fmt.Printf("INR\t\tB\n")
+		fmt.Printf("INR     B\n")
 	case 0x05:
-		fmt.Printf("DCR\t\tB\n")
+		fmt.Printf("DCR     B\n")
 	case 0x06:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x07:
 		fmt.Printf("RLC\n")
 	case 0x08:
 		fmt.Printf("NOP\n")
 	case 0x09:
-		fmt.Printf("DAD\t\tB\n")
+		fmt.Printf("DAD     B\n")
 	case 0x0A:
-		fmt.Printf("LDAX\tB\n")
+		fmt.Printf("LDAX    B\n")
 	case 0x0B:
-		fmt.Printf("DCX\t\tB\n")
+		fmt.Printf("DCX     B\n")
 	case 0x0C:
-		fmt.Printf("INR\t\tC\n")
+		fmt.Printf("INR     C\n")
 	case 0x0D:
-		fmt.Printf("DCR\t\tC\n")
+		fmt.Printf("DCR     C\n")
 	case 0x0E:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x0F:
 		fmt.Printf("RRC\n")
 	case 0x10:
 		fmt.Printf("NOP\n")
 	case 0x11:
-		fmt.Printf("LXI\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("LXI     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x12:
-		fmt.Printf("STAX\tD\n")
+		fmt.Printf("STAX    D\n")
 	case 0x13:
-		fmt.Printf("INX\t\tD\n")
+		fmt.Printf("INX     D\n")
 	case 0x14:
-		fmt.Printf("INR\t\tD\n")
+		fmt.Printf("INR     D\n")
 	case 0x15:
-		fmt.Printf("DCR\t\tD\n")
+		fmt.Printf("DCR     D\n")
 	case 0x16:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x17:
 		fmt.Printf("RAL\n")
 	case 0x18:
 		fmt.Printf("NOP\n")
 	case 0x19:
-		fmt.Printf("DAD\t\tD\n")
+		fmt.Printf("DAD     D\n")
 	case 0x1A:
-		fmt.Printf("LDAX\tD\n")
+		fmt.Printf("LDAX    D\n")
 	case 0x1B:
-		fmt.Printf("DCX\t\tD\n")
+		fmt.Printf("DCX     D\n")
 	case 0x1C:
-		fmt.Printf("INR\t\tE\n")
+		fmt.Printf("INR     E\n")
 	case 0x1D:
-		fmt.Printf("DCR\t\tE\n")
+		fmt.Printf("DCR     E\n")
 	case 0x1E:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x1F:
 		fmt.Printf("RAR\n")
 	case 0x20:
 		fmt.Printf("RIM\n")
 	case 0x21:
-		fmt.Printf("LXI\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("LXI     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x22:
-		fmt.Printf("SHLD\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("SHLD    B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x23:
-		fmt.Printf("INX\t\tH\n")
+		fmt.Printf("INX     H\n")
 	case 0x24:
-		fmt.Printf("INR\t\tH\n")
+		fmt.Printf("INR     H\n")
 	case 0x25:
-		fmt.Printf("DCR\t\tH\n")
+		fmt.Printf("DCR     H\n")
 	case 0x26:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x27:
 		fmt.Printf("DAA\n")
 	case 0x28:
 		fmt.Printf("NOP\n")
 	case 0x29:
-		fmt.Printf("DAD\t\tH\n")
+		fmt.Printf("DAD     H\n")
 	case 0x2A:
-		fmt.Printf("LHLD\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("LHLD    B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x2B:
-		fmt.Printf("DCX\t\tH\n")
+		fmt.Printf("DCX     H\n")
 	case 0x2C:
-		fmt.Printf("INR\t\tL\n")
+		fmt.Printf("INR     L\n")
 	case 0x2D:
-		fmt.Printf("DCR\t\tL\n")
+		fmt.Printf("DCR     L\n")
 	case 0x2E:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x2F:
 		fmt.Printf("CMA\n")
 	case 0x30:
 		fmt.Printf("SIM\n")
 	case 0x31:
-		fmt.Printf("LXI\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("LXI     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x32:
-		fmt.Printf("STA\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("STA     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x33:
-		fmt.Printf("INX\t\tSP\n")
+		fmt.Printf("INX     SP\n")
 	case 0x34:
-		fmt.Printf("INR\t\tM\n")
+		fmt.Printf("INR     M\n")
 	case 0x35:
-		fmt.Printf("DCR\t\tM\n")
+		fmt.Printf("DCR     M\n")
 	case 0x36:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x37:
 		fmt.Printf("STC\n")
 	case 0x38:
 		fmt.Printf("NOP\n")
 	case 0x39:
-		fmt.Printf("DAD\t\tSP\n")
+		fmt.Printf("DAD     SP\n")
 	case 0x3A:
-		fmt.Printf("LDA\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("LDA     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0x3B:
-		fmt.Printf("DCX\t\tSP\n")
+		fmt.Printf("DCX     SP\n")
 	case 0x3C:
-		fmt.Printf("INR\t\tA\n")
+		fmt.Printf("INR     A\n")
 	case 0x3D:
-		fmt.Printf("DCR\t\tA\n")
+		fmt.Printf("DCR     A\n")
 	case 0x3E:
-		fmt.Printf("MVI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("MVI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0x3F:
 		fmt.Printf("CMC\n")
 	case 0x40:
-		fmt.Printf("MOV\t\tB,B\n")
+		fmt.Printf("MOV     B,B\n")
 	case 0x41:
-		fmt.Printf("MOV\t\tB,C\n")
+		fmt.Printf("MOV     B,C\n")
 	case 0x42:
-		fmt.Printf("MOV\t\tB,D\n")
+		fmt.Printf("MOV     B,D\n")
 	case 0x43:
-		fmt.Printf("MOV\t\tB,E\n")
+		fmt.Printf("MOV     B,E\n")
 	case 0x44:
-		fmt.Printf("MOV\t\tB,H\n")
+		fmt.Printf("MOV     B,H\n")
 	case 0x45:
-		fmt.Printf("MOV\t\tB,L\n")
+		fmt.Printf("MOV     B,L\n")
 	case 0x46:
-		fmt.Printf("MOV\t\tB,M\n")
+		fmt.Printf("MOV     B,M\n")
 	case 0x47:
-		fmt.Printf("MOV\t\tB,A\n")
+		fmt.Printf("MOV     B,A\n")
 	case 0x48:
-		fmt.Printf("MOV\t\tC,B\n")
+		fmt.Printf("MOV     C,B\n")
 	case 0x49:
-		fmt.Printf("MOV\t\tC,C\n")
+		fmt.Printf("MOV     C,C\n")
 	case 0x4A:
-		fmt.Printf("MOV\t\tC,D\n")
+		fmt.Printf("MOV     C,D\n")
 	case 0x4B:
-		fmt.Printf("MOV\t\tC,E\n")
+		fmt.Printf("MOV     C,E\n")
 	case 0x4C:
-		fmt.Printf("MOV\t\tC,H\n")
+		fmt.Printf("MOV     C,H\n")
 	case 0x4D:
-		fmt.Printf("MOV\t\tC,L\n")
+		fmt.Printf("MOV     C,L\n")
 	case 0x4E:
-		fmt.Printf("MOV\t\tC,M\n")
+		fmt.Printf("MOV     C,M\n")
 	case 0x4F:
-		fmt.Printf("MOV\t\tC,A\n")
+		fmt.Printf("MOV     C,A\n")
 	case 0x50:
-		fmt.Printf("MOV\t\tD,B\n")
+		fmt.Printf("MOV     D,B\n")
 	case 0x51:
-		fmt.Printf("MOV\t\tD,C\n")
+		fmt.Printf("MOV     D,C\n")
 	case 0x52:
-		fmt.Printf("MOV\t\tD,D\n")
+		fmt.Printf("MOV     D,D\n")
 	case 0x53:
-		fmt.Printf("MOV\t\tD,E\n")
+		fmt.Printf("MOV     D,E\n")
 	case 0x54:
-		fmt.Printf("MOV\t\tD,H\n")
+		fmt.Printf("MOV     D,H\n")
 	case 0x55:
-		fmt.Printf("MOV\t\tD,L\n")
+		fmt.Printf("MOV     D,L\n")
 	case 0x56:
-		fmt.Printf("MOV\t\tD,M\n")
+		fmt.Printf("MOV     D,M\n")
 	case 0x57:
-		fmt.Printf("MOV\t\tD,A\n")
+		fmt.Printf("MOV     D,A\n")
 	case 0x58:
-		fmt.Printf("MOV\t\tE,B\n")
+		fmt.Printf("MOV     E,B\n")
 	case 0x59:
-		fmt.Printf("MOV\t\tE,C\n")
+		fmt.Printf("MOV     E,C\n")
 	case 0x5A:
-		fmt.Printf("MOV\t\tE,D\n")
+		fmt.Printf("MOV     E,D\n")
 	case 0x5B:
-		fmt.Printf("MOV\t\tE,E\n")
+		fmt.Printf("MOV     E,E\n")
 	case 0x5C:
-		fmt.Printf("MOV\t\tE,H\n")
+		fmt.Printf("MOV     E,H\n")
 	case 0x5D:
-		fmt.Printf("MOV\t\tE,L\n")
+		fmt.Printf("MOV     E,L\n")
 	case 0x5E:
-		fmt.Printf("MOV\t\tE,M\n")
+		fmt.Printf("MOV     E,M\n")
 	case 0x5F:
-		fmt.Printf("MOV\t\tE,A\n")
+		fmt.Printf("MOV     E,A\n")
 	case 0x60:
-		fmt.Printf("MOV\t\tH,B\n")
+		fmt.Printf("MOV     H,B\n")
 	case 0x61:
-		fmt.Printf("MOV\t\tH,C\n")
+		fmt.Printf("MOV     H,C\n")
 	case 0x62:
-		fmt.Printf("MOV\t\tH,D\n")
+		fmt.Printf("MOV     H,D\n")
 	case 0x63:
-		fmt.Printf("MOV\t\tH,E\n")
+		fmt.Printf("MOV     H,E\n")
 	case 0x64:
-		fmt.Printf("MOV\t\tH,H\n")
+		fmt.Printf("MOV     H,H\n")
 	case 0x65:
-		fmt.Printf("MOV\t\tH,L\n")
+		fmt.Printf("MOV     H,L\n")
 	case 0x66:
-		fmt.Printf("MOV\t\tH,M\n")
+		fmt.Printf("MOV     H,M\n")
 	case 0x67:
-		fmt.Printf("MOV\t\tH,A\n")
+		fmt.Printf("MOV     H,A\n")
 	case 0x68:
-		fmt.Printf("MOV\t\tL,B\n")
+		fmt.Printf("MOV     L,B\n")
 	case 0x69:
-		fmt.Printf("MOV\t\tL,C\n")
+		fmt.Printf("MOV     L,C\n")
 	case 0x6A:
-		fmt.Printf("MOV\t\tL,D\n")
+		fmt.Printf("MOV     L,D\n")
 	case 0x6B:
-		fmt.Printf("MOV\t\tL,E\n")
+		fmt.Printf("MOV     L,E\n")
 	case 0x6C:
-		fmt.Printf("MOV\t\tL,H\n")
+		fmt.Printf("MOV     L,H\n")
 	case 0x6D:
-		fmt.Printf("MOV\t\tL,L\n")
+		fmt.Printf("MOV     L,L\n")
 	case 0x6E:
-		fmt.Printf("MOV\t\tL,M\n")
+		fmt.Printf("MOV     L,M\n")
 	case 0x6F:
-		fmt.Printf("MOV\t\tL,A\n")
+		fmt.Printf("MOV     L,A\n")
 	case 0x70:
-		fmt.Printf("MOV\t\tM,B\n")
+		fmt.Printf("MOV     M,B\n")
 	case 0x71:
-		fmt.Printf("MOV\t\tM,C\n")
+		fmt.Printf("MOV     M,C\n")
 	case 0x72:
-		fmt.Printf("MOV\t\tM,D\n")
+		fmt.Printf("MOV     M,D\n")
 	case 0x73:
-		fmt.Printf("MOV\t\tM,E\n")
+		fmt.Printf("MOV     M,E\n")
 	case 0x74:
-		fmt.Printf("MOV\t\tM,H\n")
+		fmt.Printf("MOV     M,H\n")
 	case 0x75:
-		fmt.Printf("MOV\t\tM,L\n")
+		fmt.Printf("MOV     M,L\n")
 	case 0x76:
 		fmt.Printf("HLT\n")
 	case 0x77:
-		fmt.Printf("MOV\t\tM,A\n")
+		fmt.Printf("MOV     M,A\n")
 	case 0x78:
-		fmt.Printf("MOV\t\tA,B\n")
+		fmt.Printf("MOV     A,B\n")
 	case 0x79:
-		fmt.Printf("MOV\t\tA,C\n")
+		fmt.Printf("MOV     A,C\n")
 	case 0x7A:
-		fmt.Printf("MOV\t\tA,D\n")
+		fmt.Printf("MOV     A,D\n")
 	case 0x7B:
-		fmt.Printf("MOV\t\tA,E\n")
+		fmt.Printf("MOV     A,E\n")
 	case 0x7C:
-		fmt.Printf("MOV\t\tA,H\n")
+		fmt.Printf("MOV     A,H\n")
 	case 0x7D:
-		fmt.Printf("MOV\t\tA,L\n")
+		fmt.Printf("MOV     A,L\n")
 	case 0x7E:
-		fmt.Printf("MOV\t\tA,M\n")
+		fmt.Printf("MOV     A,M\n")
 	case 0x7F:
-		fmt.Printf("MOV\t\tA,A\n")
+		fmt.Printf("MOV     A,A\n")
 	case 0x80:
-		fmt.Printf("ADD\t\tB\n")
+		fmt.Printf("ADD     B\n")
 	case 0x81:
-		fmt.Printf("ADD\t\tC\n")
+		fmt.Printf("ADD     C\n")
 	case 0x82:
-		fmt.Printf("ADD\t\tD\n")
+		fmt.Printf("ADD     D\n")
 	case 0x83:
-		fmt.Printf("ADD\t\tE\n")
+		fmt.Printf("ADD     E\n")
 	case 0x84:
-		fmt.Printf("ADD\t\tH\n")
+		fmt.Printf("ADD     H\n")
 	case 0x85:
-		fmt.Printf("ADD\t\tL\n")
+		fmt.Printf("ADD     L\n")
 	case 0x86:
-		fmt.Printf("ADD\t\tM\n")
+		fmt.Printf("ADD     M\n")
 	case 0x87:
-		fmt.Printf("ADD\t\tA\n")
+		fmt.Printf("ADD     A\n")
 	case 0x88:
-		fmt.Printf("ADC\t\tB\n")
+		fmt.Printf("ADC     B\n")
 	case 0x89:
-		fmt.Printf("ADC\t\tC\n")
+		fmt.Printf("ADC     C\n")
 	case 0x8A:
-		fmt.Printf("ADC\t\tD\n")
+		fmt.Printf("ADC     D\n")
 	case 0x8B:
-		fmt.Printf("ADC\t\tE\n")
+		fmt.Printf("ADC     E\n")
 	case 0x8C:
-		fmt.Printf("ADC\t\tH\n")
+		fmt.Printf("ADC     H\n")
 	case 0x8D:
-		fmt.Printf("ADC\t\tL\n")
+		fmt.Printf("ADC     L\n")
 	case 0x8E:
-		fmt.Printf("ADC\t\tM\n")
+		fmt.Printf("ADC     M\n")
 	case 0x8F:
-		fmt.Printf("ADC\t\tA\n")
+		fmt.Printf("ADC     A\n")
 	case 0x90:
-		fmt.Printf("SUB\t\tB\n")
+		fmt.Printf("SUB     B\n")
 	case 0x91:
-		fmt.Printf("SUB\t\tC\n")
+		fmt.Printf("SUB     C\n")
 	case 0x92:
-		fmt.Printf("SUB\t\tD\n")
+		fmt.Printf("SUB     D\n")
 	case 0x93:
-		fmt.Printf("SUB\t\tE\n")
+		fmt.Printf("SUB     E\n")
 	case 0x94:
-		fmt.Printf("SUB\t\tH\n")
+		fmt.Printf("SUB     H\n")
 	case 0x95:
-		fmt.Printf("SUB\t\tL\n")
+		fmt.Printf("SUB     L\n")
 	case 0x96:
-		fmt.Printf("SUB\t\tM\n")
+		fmt.Printf("SUB     M\n")
 	case 0x97:
-		fmt.Printf("SUB\t\tA\n")
+		fmt.Printf("SUB     A\n")
 	case 0x98:
-		fmt.Printf("SBB\t\tB\n")
+		fmt.Printf("SBB     B\n")
 	case 0x99:
-		fmt.Printf("SBB\t\tC\n")
+		fmt.Printf("SBB     C\n")
 	case 0x9A:
-		fmt.Printf("SBB\t\tD\n")
+		fmt.Printf("SBB     D\n")
 	case 0x9B:
-		fmt.Printf("SBB\t\tE\n")
+		fmt.Printf("SBB     E\n")
 	case 0x9C:
-		fmt.Printf("SBB\t\tH\n")
+		fmt.Printf("SBB     H\n")
 	case 0x9D:
-		fmt.Printf("SBB\t\tL\n")
+		fmt.Printf("SBB     L\n")
 	case 0x9E:
-		fmt.Printf("SBB\t\tM\n")
+		fmt.Printf("SBB     M\n")
 	case 0x9F:
-		fmt.Printf("SBB\t\tA\n")
+		fmt.Printf("SBB     A\n")
 	case 0xA0:
-		fmt.Printf("ANA\t\tB\n")
+		fmt.Printf("ANA     B\n")
 	case 0xA1:
-		fmt.Printf("ANA\t\tC\n")
+		fmt.Printf("ANA     C\n")
 	case 0xA2:
-		fmt.Printf("ANA\t\tD\n")
+		fmt.Printf("ANA     D\n")
 	case 0xA3:
-		fmt.Printf("ANA\t\tE\n")
+		fmt.Printf("ANA     E\n")
 	case 0xA4:
-		fmt.Printf("ANA\t\tH\n")
+		fmt.Printf("ANA     H\n")
 	case 0xA5:
-		fmt.Printf("ANA\t\tL\n")
+		fmt.Printf("ANA     L\n")
 	case 0xA6:
-		fmt.Printf("ANA\t\tM\n")
+		fmt.Printf("ANA     M\n")
 	case 0xA7:
-		fmt.Printf("ANA\t\tA\n")
+		fmt.Printf("ANA     A\n")
 	case 0xA8:
-		fmt.Printf("XRA\t\tB\n")
+		fmt.Printf("XRA     B\n")
 	case 0xA9:
-		fmt.Printf("XRA\t\tC\n")
+		fmt.Printf("XRA     C\n")
 	case 0xAA:
-		fmt.Printf("XRA\t\tD\n")
+		fmt.Printf("XRA     D\n")
 	case 0xAB:
-		fmt.Printf("XRA\t\tE\n")
+		fmt.Printf("XRA     E\n")
 	case 0xAC:
-		fmt.Printf("XRA\t\tH\n")
+		fmt.Printf("XRA     H\n")
 	case 0xAD:
-		fmt.Printf("XRA\t\tL\n")
+		fmt.Printf("XRA     L\n")
 	case 0xAE:
-		fmt.Printf("XRA\t\tM\n")
+		fmt.Printf("XRA     M\n")
 	case 0xAF:
-		fmt.Printf("XRA\t\tA\n")
+		fmt.Printf("XRA     A\n")
 	case 0xB0:
-		fmt.Printf("ORA\t\tB\n")
+		fmt.Printf("ORA     B\n")
 	case 0xB1:
-		fmt.Printf("ORA\t\tC\n")
+		fmt.Printf("ORA     C\n")
 	case 0xB2:
-		fmt.Printf("ORA\t\tD\n")
+		fmt.Printf("ORA     D\n")
 	case 0xB3:
-		fmt.Printf("ORA\t\tE\n")
+		fmt.Printf("ORA     E\n")
 	case 0xB4:
-		fmt.Printf("ORA\t\tH\n")
+		fmt.Printf("ORA     H\n")
 	case 0xB5:
-		fmt.Printf("ORA\t\tL\n")
+		fmt.Printf("ORA     L\n")
 	case 0xB6:
-		fmt.Printf("ORA\t\tM\n")
+		fmt.Printf("ORA     M\n")
 	case 0xB7:
-		fmt.Printf("ORA\t\tA\n")
+		fmt.Printf("ORA     A\n")
 	case 0xB8:
-		fmt.Printf("CMP\t\tB\n")
+		fmt.Printf("CMP     B\n")
 	case 0xB9:
-		fmt.Printf("CMP\t\tC\n")
+		fmt.Printf("CMP     C\n")
 	case 0xBA:
-		fmt.Printf("CMP\t\tD\n")
+		fmt.Printf("CMP     D\n")
 	case 0xBB:
-		fmt.Printf("CMP\t\tE\n")
+		fmt.Printf("CMP     E\n")
 	case 0xBC:
-		fmt.Printf("CMP\t\tH\n")
+		fmt.Printf("CMP     H\n")
 	case 0xBD:
-		fmt.Printf("CMP\t\tL\n")
+		fmt.Printf("CMP     L\n")
 	case 0xBE:
-		fmt.Printf("CMP\t\tM\n")
+		fmt.Printf("CMP     M\n")
 	case 0xBF:
-		fmt.Printf("CMP\t\tA\n")
+		fmt.Printf("CMP     A\n")
 	case 0xC0:
 		fmt.Printf("RNZ\n")
 	case 0xC1:
-		fmt.Printf("POP\t\tB\n")
+		fmt.Printf("POP     B\n")
 	case 0xC2:
-		fmt.Printf("JNZ\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JNZ     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xC3:
-		fmt.Printf("JMP\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JMP     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xC4:
-		fmt.Printf("CNZ\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CNZ     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xC5:
-		fmt.Printf("PUSH\tB\n")
+		fmt.Printf("PUSH    B\n")
 	case 0xC6:
-		fmt.Printf("ADI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("ADI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xC7:
-		fmt.Printf("RST\t\t0\n")
+		fmt.Printf("RST     0\n")
 	case 0xC8:
 		fmt.Printf("RZ\n")
 	case 0xC9:
 		fmt.Printf("RET\n")
 	case 0xCA:
-		fmt.Printf("JZ\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JZ      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xCB:
 		fmt.Printf("NOP\n")
 	case 0xCC:
-		fmt.Printf("CZ\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CZ      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xCD:
-		fmt.Printf("CALL\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CALL    B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xCE:
-		fmt.Printf("ACI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("ACI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xCF:
-		fmt.Printf("RST\t\t1\n")
+		fmt.Printf("RST     1\n")
 	case 0xD0:
 		fmt.Printf("RNC\n")
 	case 0xD1:
-		fmt.Printf("POP\t\tD\n")
+		fmt.Printf("POP     D\n")
 	case 0xD2:
-		fmt.Printf("JNC\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JNC     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xD3:
-		fmt.Printf("OUT\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("OUT     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xD4:
-		fmt.Printf("CNC\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CNC     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xD5:
-		fmt.Printf("PUSH\tD\n")
+		fmt.Printf("PUSH    D\n")
 	case 0xD6:
-		fmt.Printf("SUI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("SUI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xD7:
-		fmt.Printf("RST\t\t2\n")
+		fmt.Printf("RST     2\n")
 	case 0xD8:
 		fmt.Printf("RC\n")
 	case 0xD9:
 		fmt.Printf("NOP\n")
 	case 0xDA:
-		fmt.Printf("JC\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JC      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xDB:
-		fmt.Printf("IN\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("IN      B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xDC:
-		fmt.Printf("CC\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CC      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xDD:
 		fmt.Printf("NOP\n")
 	case 0xDE:
-		fmt.Printf("SBI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("SBI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xDF:
-		fmt.Printf("RST\t\t3\n")
+		fmt.Printf("RST     3\n")
 	case 0xE0:
 		fmt.Printf("RPO\n")
 	case 0xE1:
-		fmt.Printf("POP\t\tH\n")
+		fmt.Printf("POP     H\n")
 	case 0xE2:
-		fmt.Printf("JPO\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JPO     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xE3:
 		fmt.Printf("XTHL\n")
 	case 0xE4:
-		fmt.Printf("CPO\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CPO     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xE5:
-		fmt.Printf("PUSH\tH\n")
+		fmt.Printf("PUSH    H\n")
 	case 0xE6:
-		fmt.Printf("ANI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("ANI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xE7:
-		fmt.Printf("RST\t\t4\n")
+		fmt.Printf("RST     4\n")
 	case 0xE8:
 		fmt.Printf("RPE\n")
 	case 0xE9:
 		fmt.Printf("PCHL\n")
 	case 0xEA:
-		fmt.Printf("JPE\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JPE     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xEB:
 		fmt.Printf("XCHG\n")
 	case 0xEC:
-		fmt.Printf("CPE\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CPE     B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xED:
 		fmt.Printf("NOP\n")
 	case 0xEE:
-		fmt.Printf("XRI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("XRI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xEF:
-		fmt.Printf("RST\t\t5\n")
+		fmt.Printf("RST     5\n")
 	case 0xF0:
 		fmt.Printf("RP\n")
 	case 0xF1:
-		fmt.Printf("POP\t\tPSW\n")
+		fmt.Printf("POP     PSW\n")
 	case 0xF2:
-		fmt.Printf("JP\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JP      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xF3:
 		fmt.Printf("DI\n")
 	case 0xF4:
-		fmt.Printf("CP\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CP      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xF5:
-		fmt.Printf("PUSH\tPSW\n")
+		fmt.Printf("PUSH    PSW\n")
 	case 0xF6:
-		fmt.Printf("ORI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("ORI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xF7:
-		fmt.Printf("RST\t\t6\n")
+		fmt.Printf("RST     6\n")
 	case 0xF8:
 		fmt.Printf("RM\n")
 	case 0xF9:
 		fmt.Printf("SPHL\n")
 	case 0xFA:
-		fmt.Printf("JM\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("JM      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xFB:
 		fmt.Printf("EI\n")
 	case 0xFC:
-		fmt.Printf("CM\t\tB,D16\t$%02X%02X\n", buf[2], buf[1])
+		fmt.Printf("CM      B,D16\t$%02X%02X\n", buf[2], buf[1])
 		opbytes = 3
 	case 0xFD:
 		fmt.Printf("NOP\n")
 	case 0xFE:
-		fmt.Printf("CPI\t\tB,D8\t$%02X\n", buf[1])
+		fmt.Printf("CPI     B,D8\t$%02X\n", buf[1])
 		opbytes = 2
 	case 0xFF:
-		fmt.Printf("RST\t\t7\n")
+		fmt.Printf("RST     7\n")
 	default:
 		fmt.Printf("Unknown OpCode: 0x%02X\n", buf[0])
 	}
